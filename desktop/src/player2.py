824b7c4841bda08e75ae2ae2e66be25340e91d06
Syriaker <chernetsov1130@gmail.com>
2023-12-23 17:41:28 +0300

    plaeywebfskdj

--- desktop/src/player2.py
+++ desktop/src/player2.py
@@ -1,43 +1,81 @@
-import sys, os
-from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtCore import QEvent
 from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
 from pygame import mixer
-from pathlib import Path
-paused = False
-app = QApplication([])
-main_win = QWidget()
-layout = QGridLayout()
-main_win.setLayout(layout)
-file_list = QListWidget()
-lbl = QLabel()
-file_choice = QPushButton('Выбрать файл')
-play = QPushButton('Начать трек')
-pause = QPushButton('▋▋')
-main_win.resize(800, 400)
-main_win.setWindowTitle('Player')
-mixer.init()
-def choosefile():
-    global path
-    filename = QFileDialog.getOpenFileName()
-    path = filename[0]
-def playMusic():
-    global path
-    mixer.music.load(path)
-    mixer.music.play()
-def pauseMusic():
-    global paused
-    if not paused:
-        mixer.music.pause()
-        paused = True
-        pause.setText('►')
-    else:
-        mixer.music.unpause()
-        paused = False
-        pause.setText('▋▋')
+import sys
+import icon
+import json
+import core
+import time
 
-file_choice.clicked.connect(choosefile)
-play.clicked.connect(playMusic)
-pause.clicked.connect(pauseMusic)
 
-main_win.show()
-app.exec_()
+class Player():
+    def choose_file(self):
+        global path
+        filename = QFileDialog.getOpenFileName(filter=('*.mp3'))
+        path = filename[0]
+        mixer.music.load(path)
+        mixer.music.play()
+        self.core.emotion_detector.read_data()
+        #self.lenght = mixer.music.get_pos()
+        self.core.emotion_detector.stop_read_data()
+        if (self.core.emotion_detector.get_data())[0] > (self.core.emotion_detector.get_data())[1]:
+            print('релакс')
+        else:
+            print('концентрация')
+
+
+    def pauseMusic(self):
+        if not self.paused:
+            mixer.music.pause()
+            self.paused = True
+        else:
+            mixer.music.unpause()
+            self.paused = False
+        self.core.emotion_detector.stop_read_data()
+        print(self.core.emotion_detector.get_data())
+
+
+
+    def volume_change(self):
+        volume = self.ui.music_slider.value() / 100
+        mixer.music.set_volume(volume)
+    def analysis(self):
+        print("ку")
+
+    def __init__(self):
+        self.core = core.Core()
+        app = QtWidgets.QApplication(sys.argv)
+
+        self.core.emotion_detector.start_sensors_search()
+        time.sleep(10)
+        self.core.emotion_detector.stop_sensors_search()
+        for si in self.core.emotion_detector.get_sensors_info_list():
+            if si.SerialNumber == "132007":
+                self.core.emotion_detector.connect_to_sensor(si)
+        else:
+            if self.core.emotion_detector.current_sensor is None:
+                print("no sensor with id 132007")
+                sys.exit(1)
+
+        self.core.emotion_detector.get_current_sensor_resistence()
+
+        Form = QtWidgets.QWidget()
+        mixer.init()
+        self.paused = False
+        self.ui = icon.Ui_Form()
+        self.ui.setupUi(Form)
+        self.ui.add_music.clicked.connect(self.choose_file)
+        self.ui.pauseButton.clicked.connect(self.pauseMusic)
+        self.ui.music_slider.setValue(100)
+        self.ui.music_slider.valueChanged.connect(self.volume_change)
+        Form.show()
+
+        ec = app.exec_()
+        del self.core
+        sys.exit(ec)
+
+
+
+
+Player()
