adc8d7ce50b2f68b4ab3630468437793ce08c49e
Школа 35 <lastp508@gmail.com>
2023-12-24 10:27:05 +0300

    data i/o

--- core/src/data.py
+++ core/src/data.py
@@ -1,4 +1,5 @@
 import json
+import pathlib
 
 
 class MusicList:
@@ -7,14 +8,28 @@ class MusicList:
         self.file: str = "user_data.json"
 
     def read(self):
-        file = open(self.file, "w")
-        self.data = json.load(file)
-        file.close()
 
+        if not pathlib.Path(self.file).exists():
+            self.data = {"tracks": []}
+        else:
+            file = open(self.file, "r")
+            self.data = json.load(file)
+            file.close()
+        print(self.data)
 
     def write(self):
         file = open(self.file, "w")
         json.dump(self.data, file)
         file.close()
 
-    def push_track(self, file: str, name: str,
+    def push_track(self, file: str, name: str, track_type: int):
+        if self.data == None:
+            self.data = {"tracks": []}
+        self.data["tracks"].append({"name": name, "file": file, "type": track_type})
+
+if __name__ == '__main__':
+    lst = MusicList()
+
+    lst.read()
+    lst.push_track("soviet anthem", "svtatm.mp3", 1)
+    lst.write()
